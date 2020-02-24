805c4d4ca02c3775017d8cfca3bf68325552e6ee
Irina <iryna.mosiiash@gmail.com>
2020-02-24 15:09:59 +0200

    исправление задания

--- exercise 1/script.js
+++ exercise 1/script.js
@@ -1,26 +1,23 @@
 "use: strict";  
 
-let money,
-    time;
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false
 };
 
-let expenses = {};
+let q1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    q2 = prompt("Во сколько обойдется?"),
+    q3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    q4 = prompt("Во сколько обойдется?");
 
-let requiredMoney = prompt("Введите обязательную статью расходов в этом месяце");
-let howMuch = prompt("Во сколько обойдется");
+appData.expenses.q1 = q2;
+appData.expenses.q3 = q4;
 
-expenses.requiredMoney = howMuch;
-
-console.log(expenses);
-
-let dayBudget = howMuch / 30;
-
-console.log(dayBudget);
+alert(appData.budget / 30);
